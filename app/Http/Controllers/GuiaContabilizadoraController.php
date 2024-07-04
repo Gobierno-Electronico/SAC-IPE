7da993930be8759b764cf9272a9b678b1262e541
Gobierno-Electronico <gobelectronicoipe@outlook.com>
2024-07-04 09:39:20 -0600

    Se importaron las relaciones de la plantilla de la guía contabilizadora de ingresos

--- app/Http/Controllers/GuiaContabilizadoraController.php
+++ app/Http/Controllers/GuiaContabilizadoraController.php
@@ -30,7 +30,7 @@ class GuiaContabilizadoraController extends Controller
     public function crearGuiaContabilizadora(Request $request)
     {
         // return response()->json('Método desactivado');
-        $path = public_path('Guia/Devengado-CuentasConceptos.xlsx');
+        $path = public_path('Guia/DevEnEsp-CuentasConceptos.xlsx');
 
         // Validar que el archivo pueda ser analizado correctamente.
         if ($xlsx = SimpleXLSX::parse($path)) {
@@ -243,7 +243,7 @@ class GuiaContabilizadoraController extends Controller
     public function relacionarCuentasCuentasSeguidas(Request $request)
     {
         // return response()->json('Método desactivado');
-        $path = public_path('CuentasCuentas/Devengado-CuentasCuentasSeguidas.xlsx');
+        $path = public_path('CuentasCuentas/DevEnEsp-CuentasCuentasSeguidas.xlsx');
 
         // Validar que el archivo pueda ser analizado correctamente.
         if ($xlsx = SimpleXLSX::parse($path)) {
