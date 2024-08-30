48b51983d729e1298c47195f5ee4ada234970c8b
David Mijangos <78769160+DavidMijangos08@users.noreply.github.com>
2024-08-30 09:01:27 -0600

    Avance en comprometido de capítulo 4000 "Interacción en pantalla"

--- app/Livewire/egresos/EgresosCapitulo4ComprometidoForm.php
+++ app/Livewire/egresos/EgresosCapitulo4ComprometidoForm.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 use Livewire\Attributes\Validate;
 use Livewire\Attributes\On;
 use App\Models\Cuenta;
+use App\Models\CodigoDepartamento;
 use Log;
 use DB;
 
@@ -34,6 +35,8 @@ class EgresosCapitulo4ComprometidoForm extends Component
     #[Validate('required', message: 'Importe requerido')]
     public $importe = "";
 
+    public $PTTOEjecutar = 0;
+
     public function render() 
     {
         try{
@@ -52,8 +55,56 @@ class EgresosCapitulo4ComprometidoForm extends Component
     {
         try{
             $this->validate();
+            $this->importe = floatval(str_replace(['$', ','], "", $this->importe));
+            $this->importe = ($this->importe > 0)  ? $this->importe : "";
+            $cuenta = Cuenta::find($this->cuenta);
+            $departamento = CodigoDepartamento::find($this->selectCodigoAreaResponsable);
+            
+
+            $registro = [
+                'id' => 0,
+                'codigoArea' => $this->selectCodigoArea,
+                'observaciones' => $this->observaciones,
+                'fechaAfectacion' => $this->fechaAfectacion,
+                'areaResponsableId' => $this->selectCodigoAreaResponsable,
+                'codigoAreaResponsable' => $departamento->Codigo_completo,
+                'descripcionAreaResponsable' => $departamento->Nombre,
+                'cuentaId' => $this->cuenta,
+                'codigoCuenta' => $cuenta->Codigo_cuenta,
+                'descripcionCuenta' => $cuenta->Descripcion_cuenta,
+                'mes' => $this->mes,
+                'importe' => $this->importe,
+                'pttoEjecutar' => $this->PTTOEjecutar
+            ];
+
+            $this->dispatch('agregar-registro', registro: $registro);
+            $this->limpiar();
         }catch (\Illuminate\Validation\ValidationException $e) {
             $this->dispatch('mostrarMensaje', mensaje: $e->getMessage(), tipo: 'warning', tiempo: 3000);
+        }catch (\Throwable $th) {
+            Log::error('Ocurrió un error al agregar registro en comprometido del capítulo 4: ' . $th->getMessage());
+            $this->dispatch('mostrarMensaje', mensaje: 'Ocurrió un error al agregar el registro, contacte al área de Gobierno Electrónico', tipo: 'error', tiempo: 3000);
         }
+    }
+
+    public function limpiar()
+    {
+        $this->cuenta = "";
+        $this->selectCodigoAreaResponsable = "";
+        $this->mes = "";
+        $this->PTTOEjecutar = 0;
+        $this->importe = "";
+        $this->dispatch('limpiar');
+    }
+
+    #[On('llenar-formulario')]
+    public function llenarFormulario($datosRegistro)
+    {
+        $this->cuenta = $datosRegistro['cuenta'];
+        $this->mes = $datosRegistro['mes'];
+        $this->importe = $datosRegistro['importe'];
+        $this->selectCodigoAreaResponsable = $datosRegistro['area'];
+        $this->PTTOEjecutar = $datosRegistro['pttoEjecutar'];
+        $this->dispatch('llenarFormulario', presupuesto: $this->PTTOEjecutar, importe: $this->importe);
     }
 }
--- resources/views/livewire/egresos/egresos-capitulo4-comprometido-table.blade.php
+++ resources/views/livewire/egresos/egresos-capitulo4-comprometido-table.blade.php
@@ -51,3 +51,20 @@
         </div>
     </div>
 </div>
+
+<script>
+    window.addEventListener('cambioTotal', event => {
+        let parametros = event.__livewire.params
+        $('#total').val(parametros.total);
+        formatearImporte({id: 'total'})
+    });
+
+    window.addEventListener('llenarFormulario', event => {
+        let parametros = event.__livewire.params
+        $('#inputPTTOEjecutar').val(parametros.presupuesto);
+        $('#inputImporte').val(parametros.importe);
+        formatearImporte({id: 'inputImporte'})
+        formatearImporte({id: 'inputPTTOEjecutar'})
+    });
+
+</script>
