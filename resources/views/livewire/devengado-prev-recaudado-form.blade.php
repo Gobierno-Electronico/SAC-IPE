a9b640b77f665c11f9716b059e1136df7b11f7e9
David Mijangos <78769160+DavidMijangos08@users.noreply.github.com>
2024-09-30 10:20:38 -0600

    Se agrega validación para bloquear selección de fecha de afectación mayor a la fecha actual, esto en ingresos y egresos

--- resources/views/livewire/devengado-prev-recaudado-form.blade.php
+++ resources/views/livewire/devengado-prev-recaudado-form.blade.php
@@ -48,7 +48,7 @@
             wire:model.live="observaciones">
 
         <label for="inputFechaRegistro" class="form-label mt-3">Fecha de afectación</label>
-        <input type="date" name="inputFechaRegistro" id="inputFechaRegistro" class="form-control mb-3"
+        <input type="date" name="inputFechaRegistro" id="inputFechaRegistro" class="form-control mb-3" max="{{ now()->toDateString() }}"
             wire:model.live="fechaAfectacion">
 
         <h2 class="mt-5 mb-3">Selección de movimientos</h2>
