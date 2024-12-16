e8cc457762f7e16682c6b26ac9205c02ca84f1a7
RamssesMH <ramsses.swat@gmail.com>
2024-12-16 13:48:29 -0600

    commit para hacer pull de la correcion de solvencia en prestamos

--- app/Livewire/prestamos/PrestamosRecuperacionRecaudadoPrestamosInicialesTable.php
+++ app/Livewire/prestamos/PrestamosRecuperacionRecaudadoPrestamosInicialesTable.php
@@ -7,7 +7,13 @@ use Illuminate\Pagination\LengthAwarePaginator;
 use App\Clases\Column;
 use Livewire\Attributes\On;
 use Illuminate\Database\Eloquent\Builder;
-
+use Log;
+use DB;
+use Carbon\Carbon;
+use App\Models\InteraccionCuentaCuenta;
+use App\Models\InteraccionCuentaConcepto;
+use App\Http\Controllers\BitacoraController;
+use App\Models\Poliza;
 class PrestamosRecuperacionRecaudadoPrestamosInicialesTable extends Tabla
 {
     public $cacheData = [];
@@ -15,6 +21,7 @@ class PrestamosRecuperacionRecaudadoPrestamosInicialesTable extends Tabla
     public $perPage = 6;
     public $total = 0;
     public $totalDisponible = 0;
+
 
     public function render()
     {
@@ -37,29 +44,321 @@ class PrestamosRecuperacionRecaudadoPrestamosInicialesTable extends Tabla
     {
         return [
             Column::make('area', 'Area'),
-            Column::make('partida', 'Partida'),
             Column::make('mes', 'Mes'),
+            Column::make('partida', 'Partida'),
+            Column::make('cuentaAbono', 'Cuenta de abono'),
+            Column::make('importeAbono', 'Importe abono')->component('columns.importe'),
+            Column::make('importe', 'Importe')->component('columns.importe'),
             Column::make('movimiento', 'Movimiento'),
             Column::make('pttoEjecutar', 'PPTO por ejecutar')->component('columns.importe'),
-            Column::make('importe', 'Importe')->component('columns.importe'),
             Column::make('disponibilidad', 'Disponibilidad')->component('columns.importe'),
             Column::make('id', 'Acciones')->component('columns.accionesIngresos')
         ];
     }
 
+    #[On('agregar-registro')]
+    public function agregarRegistro($registro)
+    {
+        try{
+            if($this->verificarPresupuesto($registro)){    
+                $nuevoRegistro = [
+                    'id' => 0,
+                    'area' => $registro['codigoAreaResponsable'] . ' ' . $registro['descripcionAreaResponsable'],
+                    'partida' => $registro['codigoCuenta'] . ' ' . $registro['descripcionCuenta'],
+                    'cuentaAbono' => $registro['codigoCuentaAbono'] . ' ' . $registro['descripcionCuentaAbono'],
+                    'mes' => $registro['mes'],
+                    'movimiento' => 'OTORGAMIENTO RECAUDADO PRESTAMOS INICIALES', 
+                    'pttoEjecutar' => $registro['pttoEjecutar'],
+                    'importe' => $registro['importe'],
+                    'importeAbono' => $registro['importeAbono'],
+                    'disponibilidad' => $this->totalDisponible,
+                ];
+                array_push($this->cacheData, $nuevoRegistro);
+                array_push($this->dataCompleta, $registro);
+                $this->total = 0;
+                foreach ($this->cacheData as $key => $registro) {
+                    $this->cacheData[$key]['id'] = $key + 1; 
+                    $this->dataCompleta[$key]['id'] = $key + 1;
+                    $this->total += $registro['importe'];
+                }
+               // $this->dispatch('cambioTotal', total: $this->total);
+            }
+        }catch (\Throwable $th) {
+            Log::error('Ocurrió un error al agregar registro en recaudado préstamos iniciales del capítulo 7000: '. $th->getMessage());
+            $this->dispatch('mostrarMensaje', mensaje: 'Ocurrió un error al agregar registro, contacte al área de Gobierno Electrónico', tipo: 'error', tiempo: 3000);
+        }
+    }
+
+    public function verificarPresupuesto($registro)
+    {
+        $solvencia = $registro['pttoEjecutar'];
+        $this->totalDisponible = $solvencia - $registro['importe'];
+        $totalImportes = 0;
+
+        foreach ($this->cacheData as $movimiento){
+            if(str_contains($movimiento['area'], $registro['codigoAreaResponsable']) && $movimiento['mes'] == $registro['mes']){
+                $totalImportes += $movimiento['importe'];
+            }
+        }
+
+        if($totalImportes > 0){
+            $this->totalDisponible = $solvencia - $totalImportes - $registro['importe'];
+        }
+
+        if($this->totalDisponible < 0){
+            $this->dispatch('mostrarMensaje', mensaje: 'Presupuesto por ejecutar insuficiente', tipo: 'warning', tiempo: 3000);
+            return false;
+        }
+        return true;
+    }
+
     public function edit($id)
     {
+        try{
+            $this->recalcularDisponibilidad($id);
+            foreach ($this->dataCompleta as $key => $registro) {
+                if ($registro['id'] == $id) {
+                    $datosRegistro = [
+                        'area' => $registro['areaResponsableId'],
+                        'cuenta' => $registro['cuentaId'],
+                        'cuentaAbono' => $registro['cuentaAbonoId'],
+                        'mes' => $registro['mes'],
+                        'importe' => $registro['importe'],
+                        'importeAbono' => $registro['importeAbono'],
+                        'pttoEjecutar' => $registro['pttoEjecutar']
+                    ];
+     
+                    unset($this->dataCompleta[$key]);
+                    $this->dataCompleta = array_values($this->dataCompleta );
+                    $this->dispatch('llenar-formulario', $datosRegistro);
+                    break;
+                }
+            }
+    
+            foreach ($this->cacheData as $key => $registro) {
+                if ($registro['id'] == $id) {
+                    unset($this->cacheData[$key]);
+                    $this->cacheData = array_values($this->cacheData);
+                    break;
+                }
+            }
 
+            $totalActualizado = array_sum(array_column($this->cacheData, 'importe'));
+            $this->total = $totalActualizado;
+        }catch (\Throwable $th) {
+            Log::error('Ocurrió un error al agregar registro en recaudado préstamos iniciales del capítulo 7000: '. $th->getMessage());
+            $this->dispatch('mostrarMensaje', mensaje: 'Ocurrió un error al editar, contacte al área de Gobierno Electrónico', tipo: 'error', tiempo: 3000);
+        }
     }
 
     public function delete($id)
     {
-
+        try{
+            $this->recalcularDisponibilidad($id);
+            foreach ($this->cacheData as $key => $registro) {
+                if ($registro['id'] == $id) {
+                    unset($this->cacheData[$key]);
+                    $this->cacheData = array_values($this->cacheData);
+                    break;
+                }
+            }
+    
+            foreach ($this->dataCompleta as $key => $registro) {
+                if ($registro['id'] == $id) {
+                    unset($this->dataCompleta[$key]);
+                    $this->dataCompleta = array_values($this->dataCompleta );
+                    break;
+                }
+            }
+                
+            $totalActualizado = array_sum(array_column($this->cacheData, 'importe'));
+            $this->total = $totalActualizado;
+        }catch(\Throwable $th) {
+            Log::error('Ocurrió un error al agregar registro en recaudado préstamos iniciales del capítulo 7000: '. $th->getMessage());
+            $this->dispatch('mostrarMensaje', mensaje: 'Ocurrió un error al editar, contacte al área de Gobierno Electrónico', tipo: 'error', tiempo: 3000);
+        }
     }
+
+    public function recalcularDisponibilidad($id)
+    {   
+        $mesSeleccionado = "";
+        foreach ($this->dataCompleta as $key => $registro) {
+            if ($registro['id'] == $id) {
+                $mesSeleccionado = $registro['mes'];
+
+            }
+        }
+
+        $totalImportes = 0;
+        foreach($this->cacheData as $key => $movimiento)
+        {
+            if($movimiento['id'] != $id && str_contains($movimiento['area'], $datosSeleccionado['codigoArea']) && $movimiento['mes'] == $mesSeleccionado)
+            {
+                if($totalImportes == 0){
+                    $movimiento['disponibilidad'] = $movimiento['pttoEjecutar'] - $movimiento['importe'];
+                    $totalImportes += $movimiento['importe'];
+                }else{
+                    $movimiento['disponibilidad'] = $movimiento['pttoEjecutar'] - $totalImportes - $movimiento['importe'];
+                    $totalImportes += $movimiento['importe'];
+                }
+                $this->cacheData[$key] = $movimiento;
+            }
+        }
+    }
+
+    #[On('finalizar-registros')]
+    public function finalizarRegistros()
+    {
+        if (empty($this->cacheData)) {
+            $this->dispatch('mostrarMensaje', mensaje: 'Tabla sin registros', tipo: 'error', tiempo: 3000);
+            return;
+        }
+
+        try{
+            $numerosPolizas = Poliza::select('numero_poliza')
+            ->where('tipo_poliza', '=', 'D')
+            ->whereYear('fecha', '=', Carbon::now()->year)
+            ->distinct()
+            ->orderBy('numero_poliza')
+            ->pluck('numero_poliza')
+            ->toArray();
+            sort($numerosPolizas);
+            $this->numeroPoliza = (int)end($numerosPolizas) + 1;
+
+            $numerosEvento = Poliza::select('evento')
+            ->whereYear('fecha', '=', Carbon::now()->year)
+            ->distinct()
+            ->orderBy('evento')
+            ->pluck('evento')
+            ->toArray();
+            sort($numerosEvento);
+            $this->numeroEvento = (int)end($numerosEvento) + 1;
+
+            $anioActual = Carbon::now()->year;
+            $fecha = Carbon::now('America/Mexico_City');
+            $fecha->year($anioActual);
+
+            $bitacora = new BitacoraController();
+            $bitacora->bitacora('finalizarRegistros', 'registro o intentó registrar un recaudado préstamos inicales del capítulo 7000: '.$this->numeroEvento, request());
+            DB::beginTransaction();
+
+            foreach ($this->dataCompleta as $movimiento)
+            {
+                $movimiento['importe'] = doubleval($movimiento['importe']);
+                $movimiento['importeAbono'] = doubleval($movimiento['importeAbono']);
+
+                $interaccionCuentaConceptoPrincipal = InteraccionCuentaConcepto::where('cuenta_id', '=', $movimiento['cuentaId'])->whereIn('concepto_id', [94])
+                ->where('tipo_interaccion', '=', 'Presupuestal - Abono')->first();
+
+                $interaccionCuentaCuentas = InteraccionCuentaCuenta::where('id_interaccion_concepto_cuenta_1', '=', $interaccionCuentaConceptoPrincipal->id)
+                ->join('interaccion_cuenta_conceptos', 'interaccion_cuenta_conceptos.id', '=', 'interaccion_cuenta_cuentas.id_interaccion_concepto_cuenta_2')
+                ->join('cuentas', 'cuentas.id', '=', 'interaccion_cuenta_conceptos.cuenta_id')->get()->toArray();
+                dd($interaccionCuentaCuentas);
+
+
+
+                $polizas = [
+                    [
+                        'area' => $movimiento['codigoAreaResponsable'],
+                        'tipo_poliza' => 'D',
+                        'numero_poliza' =>  $this->numeroPoliza,
+                        'fecha' => $movimiento['fechaAfectacion'],
+                        'cuenta' => $movimiento['codigoCuenta'],
+                        'concepto' => $movimiento['descripcionCuenta'],
+                        'total' => abs($movimiento['importe']),
+                        'mes' => $movimiento['mes'],
+                        'descripcion' => $movimiento['observaciones'],
+                        'evento' => $this->numeroEvento,
+                        'tipo_interaccion' => $interaccionCuentaConceptoPrincipal->tipo_interaccion,
+                        'validado' => false,
+                        'estatus_evento' => true,
+                        'categoria' => 'OTORGAMIENTO COMPROMISO DEVENGADO PRESTAMOS INICIALES',
+                        'created_at' => $fecha,
+                        'updated_at' => $fecha
+                    ]
+                ];
+
+                array_push($polizas, [
+                    'area' => $movimiento['codigoAreaResponsable'],
+                    'tipo_poliza' => 'D',
+                    'numero_poliza' =>  $this->numeroPoliza,
+                    'fecha' => $movimiento['fechaAfectacion'],
+                    'cuenta' => $movimiento['codigoCuentaAbono'],
+                    'concepto' => $movimiento['descripcionCuentaAbono'],
+                    'total' => abs($movimiento['importeAbono']),
+                    'mes' => $movimiento['mes'],
+                    'descripcion' => $movimiento['observaciones'],
+                    'evento' => $this->numeroEvento,
+                    'tipo_interaccion' => $interaccionCuentaConceptoAbono->tipo_interaccion,
+                    'validado' => false,
+                    'estatus_evento' => true,
+                    'categoria' => 'OTORGAMIENTO COMPROMISO DEVENGADO PRESTAMOS INICIALES',
+                    'created_at' => $fecha,
+                    'updated_at' => $fecha
+                ]);
+
+                foreach ($interaccionCuentaCuentas as $key => $dataCuenta) {
+                    $total = $movimiento['importe'];
+                    if($dataCuenta['tipo_interaccion'] == 'Contable - Abono'){
+                        $total = $movimiento['importe'] - $movimiento['importeAbono'];
+                    }
+                    array_push($polizas, [
+                        'area' => $movimiento['codigoAreaResponsable'],
+                        'tipo_poliza' => 'D',
+                        'numero_poliza' =>  $this->numeroPoliza,
+                        'fecha' => $movimiento['fechaAfectacion'],
+                        'cuenta' => $dataCuenta['Codigo_cuenta'],
+                        'concepto' => $dataCuenta['Descripcion_cuenta'],
+                        'total' => $total,
+                        'mes' => $movimiento['mes'],
+                        'descripcion' => $movimiento['observaciones'],
+                        'evento' => $this->numeroEvento,
+                        'tipo_interaccion' => $dataCuenta['tipo_interaccion'],
+                        'validado' => false,
+                        'estatus_evento' => true,
+                        'categoria' => 'OTORGAMIENTO COMPROMISO DEVENGADO PRESTAMOS INICIALES',
+                        'created_at' => $fecha,
+                        'updated_at' => $fecha
+                    ]);
+                }
+
+                foreach ($interaccionCuentaCuentasAbono as $key => $dataCuenta) {
+                    array_push($polizas, [
+                        'area' => $movimiento['codigoAreaResponsable'],
+                        'tipo_poliza' => 'D',
+                        'numero_poliza' =>  $this->numeroPoliza,
+                        'fecha' => $movimiento['fechaAfectacion'],
+                        'cuenta' => $dataCuenta['Codigo_cuenta'],
+                        'concepto' => $dataCuenta['Descripcion_cuenta'],
+                        'total' => $movimiento['importeAbono'],
+                        'mes' => $movimiento['mes'],
+                        'descripcion' => $movimiento['observaciones'],
+                        'evento' => $this->numeroEvento,
+                        'tipo_interaccion' => $dataCuenta['tipo_interaccion'],
+                        'validado' => false,
+                        'estatus_evento' => true,
+                        'categoria' => 'OTORGAMIENTO COMPROMISO DEVENGADO PRESTAMOS INICIALES',
+                        'created_at' => $fecha,
+                        'updated_at' => $fecha
+                    ]);
+                }
+                
+                Poliza::insert($polizas);
+                DB::commit();
+            }
+            $this->dispatch('consultar-registro', $this->numeroEvento, $this->numeroPoliza, $this->total);
+        }catch (\Throwable $th) {
+            DB::rollBack();
+            Log::error('Ocurrió un error al finalizarRegistro en compromiso-devengado préstamos iniciales del capítulo 7000: '. $th->getMessage());
+            $this->dispatch('mostrarMensaje', mensaje: 'Ocurrió un error al realizar el registro, contacte al área de Gobierno Electrónico', tipo: 'error', tiempo: 3000);
+        }
+    }
+
 
     public function changeState($value)
     {
 
     }
+
 
 }
--- resources/views/livewire/prestamos/prestamos-recuperacion-recaudado-prestamosIniciales-form.blade.php
+++ resources/views/livewire/prestamos/prestamos-recuperacion-recaudado-prestamosIniciales-form.blade.php
@@ -22,8 +22,8 @@
             </div>
         </div>
         <livewire:prestamos.prestamos-recuperacion-recaudado-prestamosIniciales-table :$numeroPoliza :$numeroEvento :$total
-            tipoMovimiento="PolizaEgresosComprometidoCapitulo2y3" urlFinalizar="/capitulo2y3-comprometido"
-            tipoPoliza="E" categoriaModulo='EGRESOS COMPROMETIDO CAPITULO 2y3' />
+            tipoMovimiento="PolizaOtorgamientoRecaudadoPrestamosIniciales" urlFinalizar="/capitulo7-otorgamiento-recaudado-prestamosIniciales"
+            tipoPoliza="D" categoriaModulo='OTORGAMIENTO COMPROMISO RECAUDADO PRESTAMOS INICIALES' />
     @else
         <label for="selectAreaSolicitante" class="form-label">Área solicitante</label>
         <select name="selectAreaSolicitante" id="selectAreaSolicitante" class="form-select"
@@ -51,7 +51,7 @@
             <div class="col-3">
                 <label for="selectAreaResponsable" class="form-label mt-3">Área responsable</label>
                 <select name="selectAreaResponsable" id="selectAreaResponsable" class="form-select"
-                    wire:model="selectCodigoAreaResponsable" wire:change="">
+                    wire:model="selectCodigoAreaResponsable" wire:change="cargarPresupuesto">
                     <option value="" @if ($this->selectCodigoAreaResponsable == '') selected @endif disabled>
                         Seleccionar un área
                     </option> 
@@ -64,17 +64,8 @@
                     @endforeach
                 </select>
 
-                <label for="selectCuenta" class="form-label mt-3" >Cuenta</label>
-                <select name="selectCuenta" id="selectCuenta" class="form-select" wire:model="cuenta" wire:change="">
-                    <option value="" disabled>Seleccionar cuenta</option>
-                   {{--  @foreach ($cuentas as $cuenta)
-                    <option value="{{ $cuenta->cuenta_id }}"> 
-                            {{  $cuenta->Codigo_cuenta . '  ' . $cuenta->Descripcion_cuenta }}</option>
-                    @endforeach --}}
-                </select>
-
                 <label for="selectMes" class="form-label mt-3">Mes de afectación</label>
-                <select name="selectMes" id="selectMes" class="form-select" wire:model="mes" wire:change="">
+                <select name="selectMes" id="selectMes" class="form-select" wire:model="mes" wire:change="cargarPresupuesto">
                     <option value="" selected disabled>Seleccionar mes...</option>
                     @foreach (range(1, 12) as $mes)
                         @php
@@ -82,6 +73,15 @@
                         @endphp
                         <option value="{{ ucfirst($carbonMes->monthName) }}">{{ ucfirst($carbonMes->monthName) }}
                         </option>
+                    @endforeach
+                </select>
+
+                <label for="selectCuenta" class="form-label mt-3" >Cuenta</label>
+                <select name="selectCuenta" id="selectCuenta" class="form-select" wire:model="cuenta" wire:change="cargarCuentaContableAbono(true)">
+                    <option value="" disabled>Seleccionar cuenta</option>
+                    @foreach ($cuentas as $cuenta)
+                        <option value="{{ $cuenta->cuenta_id }}">
+                            {{ $cuenta->Codigo_cuenta . '  ' . $cuenta->Descripcion_cuenta }}</option>
                     @endforeach
                 </select>
 
@@ -108,3 +108,47 @@
         
     @endif
 </div>
+
+<script>
+    window.addEventListener('formato_importe', event => {
+        let params = event.__livewire.params
+        formatearImporte({
+            id: params.id
+        }, params.amount)
+    })
+
+    window.addEventListener('limpiar', event => {
+        limpiar()
+    })
+
+    function keyPress(e, obj) {
+        let isCurrency = $('#' + obj.id).val().search(/[$]/)
+        let texto = $('#' + obj.id).val().replace(/[^0-9.]/g, '');
+        let isDecimal = texto.search(/[.]/)
+        let amount = parseFloat(texto);
+        if (!isNaN(amount) && isDecimal < 0 || isCurrency == 0) {
+            $('#' + obj.id).val(amount.toLocaleString());
+        }
+    }
+
+    function formatearImporte(obj, amount = '') {
+        amount = (amount) ? amount : $('#' + obj.id).val().replace(/[^0-9.]/g, '');
+        amount = parseFloat(amount);
+        if (!isNaN(amount)) {
+            var formattedAmount = amount.toLocaleString('es-MX', {
+                style: 'currency',
+                currency: 'MXN',
+                minimumFractionDigits: 2,
+            });
+            $('#' + obj.id).val(formattedAmount);
+            console.log("Ejecuta: " + obj);
+        } else {
+            toastr.warning('Ingrese valores numéricos en el campo de importe');
+            $('#' + obj.id).val('');
+        }
+    }
+
+    function limpiar() {
+        $('#inputPTTOEjecutar').val('');
+    }
+</script>
