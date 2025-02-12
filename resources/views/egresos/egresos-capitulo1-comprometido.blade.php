e4887a20a8106b16b4260c9aa985be538a99ae40
Gobierno-Electronico <gobelectronicoipe@outlook.com>
2025-02-12 10:05:15 -0600

    corrección del largo del card en la vista de carga de comprometido y devengado en el capitulo 1000

--- resources/views/egresos/egresos-capitulo1-comprometido.blade.php
+++ resources/views/egresos/egresos-capitulo1-comprometido.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <script src="{{ asset('js/Egresos/egresos.js') }}"></script>
     <div class="row justify-content-center p-5">
-        <div class="col-md-12">
+        <div class="col-md-9">
             <div class="card shadow border-0">
                 <div class="card-body bg-white p-5">
                     <h2>Egresos capítulo 1000 Servicios Personales</h2>
