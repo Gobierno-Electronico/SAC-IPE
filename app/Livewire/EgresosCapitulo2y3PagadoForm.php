6c22a1bd685be24eb49320e50ab6b18a9e83c573
David Mijangos <78769160+DavidMijangos08@users.noreply.github.com>
2025-04-14 12:27:44 -0600

    Se agregó cargar el área automáticamente en base al evento seleccionado tanto en ingresos como en egresos

--- app/Livewire/EgresosCapitulo2y3PagadoForm.php
+++ app/Livewire/EgresosCapitulo2y3PagadoForm.php
@@ -93,18 +93,39 @@ class EgresosCapitulo2y3PagadoForm extends Component
     {
         try {
             $this->limpiar();
-            $descripcionEvento = Poliza::select('descripcion')
-            ->where('evento', '=', $this->numeroEvento)
-            ->where('tipo_poliza', '=', 'E')
-            ->where('categoria', '=', 'EGRESOS EJERCIDO CAPITULO 2y3')
-            ->get()[0]->descripcion;
-            $this->observaciones = $descripcionEvento;
+            $this->llenarCamposEspecificos();
             $this->montoDelEvento = DB::select('EXEC ImporteTotalCapitulo2y3Pagado @evento = ?', array($this->numeroEvento))[0]->MontoDelEvento;
             $this->dispatch('formato_importe', id: 'inputMontoEvento', amount: ($this->montoDelEvento > 0) ? $this->montoDelEvento : '');
             $this->dispatch('mostrarMensaje', mensaje: 'Monto del evento cargado', tipo: 'success', tiempo: 1500);
             $this->llenarPartidasPresupuestales();
         } catch (\Throwable $th) {
             Log::error('Ocurrió un error al cargar el evento en Pagado del capítulo 2000 y 3000: ' . $th->getMessage());
+            $this->dispatch('mostrarMensaje', mensaje: 'Ocurrió un error al cargar el evento, contacte al área de Gobierno Electrónico', tipo: 'error', tiempo: 3000);
+        }
+    }
+
+    public function llenarCamposEspecificos(){
+        try{      
+            $descripcionEvento = Poliza::select('descripcion')
+            ->where('evento', '=', $this->numeroEvento)
+            ->where('tipo_poliza', '=', 'E')
+            ->where('categoria', '=', 'EGRESOS EJERCIDO CAPITULO 2y3')
+            ->get()[0]->descripcion;
+        
+            $areaEvento = Poliza::select('area')
+                ->where('evento', '=', $this->numeroEvento)
+                ->where('tipo_poliza', '=', 'E')
+                ->where('categoria', '=', 'EGRESOS EJERCIDO CAPITULO 2y3')
+                ->get()[0]->area;
+        
+            $idArea = CodigoDepartamento::select('id')
+                ->where('Codigo_completo', '=', $areaEvento)
+                ->get()[0]->id; 
+        
+            $this->observaciones = $descripcionEvento;
+            $this->selectCodigoAreaResponsable = $idArea;   
+        }catch (\Throwable $th) {
+            Log::error('Ocurrió un error al llenar campos específicos en Pagado del capítulo 2 y 3: ' . $th->getMessage());
             $this->dispatch('mostrarMensaje', mensaje: 'Ocurrió un error al cargar el evento, contacte al área de Gobierno Electrónico', tipo: 'error', tiempo: 3000);
         }
     }
--- app/Livewire/EgresosCapitulo5PagadoForm.php
+++ app/Livewire/EgresosCapitulo5PagadoForm.php
@@ -96,18 +96,39 @@ class EgresosCapitulo5PagadoForm extends Component
     {
         try {
             $this->limpiar();
-            $descripcionEvento = Poliza::select('descripcion')
-            ->where('evento', '=', $this->numeroEvento)
-            ->where('tipo_poliza', '=', 'E')
-            ->where('categoria', '=', 'EGRESOS EJERCIDO CAPITULO 5')
-            ->get()[0]->descripcion;
-            $this->observaciones = $descripcionEvento;
+            $this->llenarCamposEspecificos();
             $this->montoDelEvento = DB::select('EXEC ImporteTotalCapitulo5Pagado @evento = ?', array($this->numeroEvento))[0]->MontoDelEvento;
             $this->dispatch('formato_importe', id: 'inputMontoEvento', amount: ($this->montoDelEvento > 0) ? $this->montoDelEvento : '');
             $this->dispatch('mostrarMensaje', mensaje: 'Monto del evento cargado', tipo: 'success', tiempo: 1500);
             $this->llenarPartidasPresupuestales();
         } catch (\Throwable $th) {
             Log::error('Ocurrió un error al cargar el evento en Pagado del capítulo 5000: ' . $th->getMessage());
+            $this->dispatch('mostrarMensaje', mensaje: 'Ocurrió un error al cargar el evento, contacte al área de Gobierno Electrónico', tipo: 'error', tiempo: 3000);
+        }
+    }
+
+    public function llenarCamposEspecificos(){
+        try{      
+            $descripcionEvento = Poliza::select('descripcion')
+            ->where('evento', '=', $this->numeroEvento)
+            ->where('tipo_poliza', '=', 'E')
+            ->where('categoria', '=', 'EGRESOS EJERCIDO CAPITULO 5')
+            ->get()[0]->descripcion;
+        
+            $areaEvento = Poliza::select('area')
+                ->where('evento', '=', $this->numeroEvento)
+                ->where('tipo_poliza', '=', 'E')
+                ->where('categoria', '=', 'EGRESOS EJERCIDO CAPITULO 5')
+                ->get()[0]->area;
+        
+            $idArea = CodigoDepartamento::select('id')
+                ->where('Codigo_completo', '=', $areaEvento)
+                ->get()[0]->id; 
+        
+            $this->observaciones = $descripcionEvento;
+            $this->selectCodigoAreaResponsable = $idArea;   
+        }catch (\Throwable $th) {
+            Log::error('Ocurrió un error al llenar campos específicos en Pagado del capítulo 5: ' . $th->getMessage());
             $this->dispatch('mostrarMensaje', mensaje: 'Ocurrió un error al cargar el evento, contacte al área de Gobierno Electrónico', tipo: 'error', tiempo: 3000);
         }
     }
