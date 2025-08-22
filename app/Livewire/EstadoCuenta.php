19f995a393b290660f6314385da3584362042387
David Mijangos <78769160+DavidMijangos08@users.noreply.github.com>
2025-08-22 09:56:50 -0600

    avances interfaz estado cuenta

--- app/Livewire/EstadoCuenta.php
+++ app/Livewire/EstadoCuenta.php
@@ -2,53 +2,53 @@
 
 namespace App\Livewire;
 
-use Carbon\Carbon;
 use Livewire\Component;
-use Livewire\Attributes\On;
 use Livewire\Attributes\Validate;
-use DB;
+use Livewire\Attributes\On;
 use App\Models\Cuenta;
+use Log;
 
 class EstadoCuenta extends Component
 {
-
     #[Validate('required', message: 'Cuenta requerida')]
-    public $cuenta = null;
-
+    public ?string $cuenta = null; 
+    
     #[Validate('required', message: 'Fecha de inicio requerida')]
     public $fechaInicio = '';
 
     #[Validate('required', message: 'Fecha de fin requerida')]
     public $fechaFin = '';
 
-    public $filtroDescripcion;
+    public $filtroDescripcion = '';
 
-    public $meses = ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'];
-
-
+    public $meses = ['ENERO','FEBRERO','MARZO','ABRIL','MAYO','JUNIO','JULIO','AGOSTO','SEPTIEMBRE','OCTUBRE','NOVIEMBRE','DICIEMBRE'];
 
     public function render()
     {
-        try{
-            $cuentas = Cuenta::all();
+        try {
+            $cuentas = Cuenta::orderBy('Codigo_cuenta')
+            ->whereIn('Nivel', [6, 9])
+            ->when($this->filtroDescripcion, function ($query) {
+                $query->where(function($q) {
+                    $q->where('Descripcion_cuenta', 'like', '%' . $this->filtroDescripcion . '%')
+                    ->orWhere('Codigo_cuenta', 'like', '%' . $this->filtroDescripcion . '%');
+                });
+            })
+            ->get();
             return view('livewire.estado-cuenta', ['cuentas' => $cuentas]);
-        }catch(\Throwable $th){
+        } catch (\Throwable $th) {
             Log::error('Ocurrió un error al cargar cuentas en reportes Estado de cuenta: ' . $th->getMessage());
-            $this->dispatch('mostrarMensaje', mensaje: 'Ocurrió un error al cargar las cuentas, contacte al área de Gobierno Electrónico', tipo: 'error', tiempo: 3000); 
+            $this->dispatch('mostrarMensaje', mensaje: 'Ocurrió un error al cargar las cuentas, contacte al área de Gobierno Electrónico', tipo: 'error', tiempo: 3000);
         }
-    }
-
-    public function save() {
-
-    }
-
-
-    public function change(){
     }
 
     public function generarEstadoCuenta()
     {
-        $this->validate();
-        dd($this->cuenta, $this->fechaInicio, $this->fechaFin);
+        try {
+            $this->validate();
+            dd($this->cuenta, $this->fechaInicio, $this->fechaFin);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            $this->dispatch('mostrarMensaje', mensaje: $e->getMessage(), tipo: 'warning', tiempo: 3000);
+        }
     }
 }
