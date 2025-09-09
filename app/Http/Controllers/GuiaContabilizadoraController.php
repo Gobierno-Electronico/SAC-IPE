f65650329d196a7108a877f7c03680e49aea9b39
Brandon <brandontrujillo1217@gmail.com>
2025-09-09 09:53:05 -0600

    adición de relaciones entre cuentas de recaudado

--- app/Http/Controllers/GuiaContabilizadoraController.php
+++ app/Http/Controllers/GuiaContabilizadoraController.php
@@ -32,7 +32,7 @@ class GuiaContabilizadoraController extends Controller
         set_time_limit(30000);
         ini_set('max_execution_time', 30000);
         // return response()->json('Método desactivado');
-        $path = public_path('Guia/deudores-anticipos-comprobación-CuentasConceptos.xlsx'); // cambiar el número del capítulo cuando se carguen las relaciones
+        $path = public_path('Guia/Rec-CuentasConceptos2.xlsx'); // cambiar el número del capítulo cuando se carguen las relaciones
 
         // Validar que el archivo pueda ser analizado correctamente.
         if ($xlsx = SimpleXLSX::parse($path)) {
@@ -247,7 +247,7 @@ class GuiaContabilizadoraController extends Controller
                 set_time_limit(30000);
         ini_set('max_execution_time', 30000);
         // return response()->json('Método desactivado');
-        $path = public_path('CuentasCuentas/deudores-anticipos-comprobación-CuentasSeguidas.xlsx');
+        $path = public_path('CuentasCuentas/Rec-CuentasCuentasSeguidas2.xlsx');
 
         // Validar que el archivo pueda ser analizado correctamente.
         if ($xlsx = SimpleXLSX::parse($path)) {
