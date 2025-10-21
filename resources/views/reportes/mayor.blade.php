943f60d64df72048468649bf5123b9d253727af9
Brandon <brandontrujillo1217@gmail.com>
2025-10-21 12:55:49 -0600

    Reporte de Libro mayor

--- resources/views/reportes/mayor.blade.php
+++ resources/views/reportes/mayor.blade.php
@@ -6,7 +6,7 @@
      
 
     <div class="container mt-5">
-        <div class="shadow rounded p-5">
+        <div class="shadow rounded p-5 mx-auto w-75">
             <h2 class="mb-4">Libro mayor</h2>
             <livewire:libro-mayor />
         </div>
