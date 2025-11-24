9efb883458dd19be8b23ac1a6e5f775b104b6907
David Mijangos <78769160+DavidMijangos08@users.noreply.github.com>
2025-11-24 12:27:15 -0600

    se elimina referencia de balanza armonizada en reportes de diario y mayor

--- resources/views/reportes/diario.blade.php
+++ resources/views/reportes/diario.blade.php
@@ -2,9 +2,6 @@
 @section('title', 'Libro diario')
 
 @section('content')
-    <script src="{{ asset('js/Presupuesto/balanzaArmonizada.js') }}"></script>
-     
-
     <div class="container mt-5">
         <div class="shadow rounded p-5 mx-auto w-75">
             <h2 class="mb-4">Libro diario</h2>
