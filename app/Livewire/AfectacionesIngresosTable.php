4c71469b3fbf4f720d11062bcb4f097cf6097304
David Mijangos <78769160+DavidMijangos08@users.noreply.github.com>
2026-03-05 08:28:02 -0600

    Corrección en afectaciones ingresos

--- app/Livewire/AfectacionesIngresosTable.php
+++ app/Livewire/AfectacionesIngresosTable.php
@@ -54,6 +54,8 @@ class AfectacionesIngresosTable extends Tabla
     public $total = 0;
     public $totalPrevio;
     public $totalProceso = 0;
+    public int $anio;
+
 
     public function render()
     {
@@ -84,7 +86,7 @@ class AfectacionesIngresosTable extends Tabla
                         $entrada->SolvenciaFinal = 0;
                         $entrada->PresupuestoFinal = 0;
                         return $entrada;
-                    }, DB::select('EXEC AfectacionesLiquidasTabla @area = ?, @cuenta = ?, @anio = ?', array($departamento->Codigo_completo, $cuenta->Codigo_cuenta, (string) $this->anio)));
+                    }, DB::select('EXEC AfectacionesLiquidasTabla @area = ?, @cuenta = ?, @anio = ?', array($departamento->Codigo_completo, $cuenta->Codigo_cuenta, $this->anio)));
             }
             return $this->cacheData;
         } else {
@@ -99,7 +101,7 @@ class AfectacionesIngresosTable extends Tabla
                         $entrada->SolvenciaFinal = 0;
                         $entrada->PresupuestoFinal = 0;
                         return $entrada;
-                    }, DB::select('EXEC AfectacionesLiquidasTabla @area = ?, @cuenta = ?, @anio = ?', array($departamento->Codigo_completo, $cuenta->Codigo_cuenta, (string) $this->anio)));
+                    }, DB::select('EXEC AfectacionesLiquidasTabla @area = ?, @cuenta = ?, @anio = ?', array($departamento->Codigo_completo, $cuenta->Codigo_cuenta, $this->anio)));
             }
             return $this->cacheData;
         }
