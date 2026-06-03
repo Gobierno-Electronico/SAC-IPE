4bd832883a35101c4a25f9bfa70e23414db49a43
Brandon <brandontrujillo1217@gmail.com>
2026-06-03 14:21:36 -0600

    cambios en el blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,6 @@
 
     <title>@yield('title')</title>
 
-    <link rel="stylesheet" href="{{ asset('vendor/toastr/toastr.min.css') }}">
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('vendor/toastr/toastr.min.js') }}"></script>
     <script src="{{ asset('vendor/dayjs/dayjs.min.js') }}"></script>
@@ -25,6 +24,7 @@
 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @vite(['resources/css/layouts/app.css', 'resources/css/layouts/loading.css', 'resources/css/layouts/loadingDots.css'])
+    <link rel="stylesheet" href="{{ asset('vendor/toastr/toastr.min.css') }}">
     <script>
         window.añoSelect = null;
     </script>
